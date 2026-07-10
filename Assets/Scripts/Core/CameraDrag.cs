5ba67af1a79045a9fb6d32f94d3e85abe8b7b873
RafiDevari <rafidevari2004@gmail.com>
2026-07-10 12:03:47 +0700

    add scrollable camera To Do : add fix on click on popup so it will appear onrelease, not on click

---
+++ Assets/Scripts/Core/CameraDrag.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CameraDrag : MonoBehaviour
+{
+    private Vector3 dragOrigin;
+
+    
+
+    void Update()
+    {   
+        // Zoom dengan scroll mouse
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll != 0)
+        {
+            Camera.main.orthographicSize -= scroll * 5f;
+            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 2f, 15f);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 difference = dragOrigin - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position += difference;
+        }
+    }
+}
