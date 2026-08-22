322cf32ce5487e47b1aa5074fe5c9849870145f3
IbraRizqy <rizqyibra@gmail.com>
2026-08-22 11:36:55 +0700

    Add Credits scene and update UI navigation
    - Created a new Credits scene with associated metadata.
    - Updated Gallery and MainMenu scenes to include back button.
    - Implemented SceneLoader script for managing scene transitions.
    - Adjusted build settings to include the new Credits scene and Shop scene.

---
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigation : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
